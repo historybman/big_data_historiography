0c86b23f9201894459a980f85b3d4eefb8a53e78
Bob Morrissey <rmorriss@illinois.edu>
2017-05-03 22:21:46 -0500

    wrote functions for parsing xml after crawling file structure.

---
+++ code/file_crawler_parser.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 10:34:46 2017
+
+@author: rmorriss
+"""
+
+#import os
+#
+#rootdir ='../data/jah_data'
+#
+#for subdir, dirs, files in os.walk(rootdir):
+#    for file in files:
+#        if 
+#        f=open(file,'r')
+#        lines=f.readlines()
+#        f.close()
+#        f=open(file,'w')
+#        for line in lines:
+#            
+#        f.close()
+        
+import glob
+ngrams1 = glob.glob('../data/**/*NGRAMS1.txt', recursive = True)
+xml = glob.glob('../data/**/*.xml', recursive = True)
+print(len(ngrams1), len(xml))
+#print(xml)
+
+dictionaries= []
+for ngram in ngrams1:
+    fhand = open(ngram, 'r')
+    test = fhand.readlines()
+    fhand.close()
+    dic = {}
+    for line in test:
+        line2 = line.split()
+        key = line2[0]
+        val = int(line2[1])
+        dic[key] = val
+    dictionaries.append(dic)
+    
+print(len(dictionaries), dictionaries[0])
+
+# Put this in thinking that I needed to open and read the file;
+# In fact, etree.parse takes the name of the file, not the string.
+#def open_xml(file):
+#    fhand = open(file)
+#    xml_file = fhand.readlines()
+#    fhand.close()
+#    print("read file")
+#    return xml_file
+
+
+def xml_parse(file):
+    # Function to parse the xml files and return the root
+    # from here you can navigate and pull out data from the metadata
+    import xml.etree.ElementTree as etree
+    tree = etree.parse(file)
+    print("parsed file")
+    root = tree.getroot()
+    print(root)
+
+count = 0    
+for x in xml:
+#    opened = open_xml(x)
+    xml_parse(x)
+    count += 1
+    print(count)
+    
+
+
